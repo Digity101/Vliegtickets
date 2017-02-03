f89de6b6b02bab2892f081f96a7a06cd74a2c504
MikaZeilstra <mika.zeilstra@gmail.com>
2017-02-03 14:57:49 +0100

    Nieuwe Inputs

--- index.php
+++ index.php
@@ -4,32 +4,35 @@ include 'config.php';
 <!DOCTYPE html>
 <html lang="nl">
 
-<head>
-    <title>Geen scam</title>
-    <link href="icon.ico" rel="shortcut icon">
-    <meta charset="UTF-8">
-    <link href="/styles.css" rel="stylesheet">
-</head>
+    <head>
+        <title>Geen scam</title>
+        <link href="icon.ico" rel="shortcut icon">
+        <meta charset="UTF-8">
+        <link href="/styles.css" rel="stylesheet">
+    </head>
 
-<body>
-  Koop hier goedkope vliegtickets
-<form action="idk.php" method="post">
-  <input list="landen">
-<datalist id="landen">
-    <?php
-  $query = mysqli_query($conn,"SELECT Land FROM myTable");
-  $land = mysqli_fetch_all($query,MYSQLI_ASSOC);
-  foreach ($land as $key => $value) {
-    foreach ($value as $key => $v) {
-      echo "<option value='".$v."'>";
-    }
-  }
-  ?>
-</datalist>
-  <input type="submit">
-</form>
+    <body>
+        Koop hier goedkope vliegtickets
+        <form action="idk.php" method="post">
+            <input list="landen" name="Vertrek">
+            <input list="landen" name="Aantkomst">
+            <datalist id="landen">
+                <?php
+                $query = mysqli_query($conn,"SELECT Land FROM myTable");
+                $land = mysqli_fetch_all($query,MYSQLI_ASSOC);
+                foreach ($land as $key => $value) {
+                    foreach ($value as $key => $v) {
+                        echo "<option value='".$v."'>";
+                    }
+                }
+                ?>
+            </datalist>
+            <input type="date" name="DatumVlucht">
+            
+            <input type="submit" name="Zoek een vlucht">
+        </form>
 
 
-</body>
+    </body>
 
 </html>
