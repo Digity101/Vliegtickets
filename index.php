e8b35e9fa400f82835bd6a71936a9433f9093608
MikaZ <mika.zeilstra@gmail.com>
2017-02-09 18:16:37 +0100

    Retour Hide

--- index.php
+++ index.php
@@ -4,32 +4,33 @@ include 'config.php';
 <!DOCTYPE html>
 <html lang="nl">
 
-<head>
-    <title>Geen scam</title>
-    <link href="icon.ico" rel="shortcut icon">
-    <meta charset="UTF-8">
-    <link href="/styles.css" rel="stylesheet">
-</head>
+    <head>
+        <title>Geen scam</title>
+        <link href="icon.ico" rel="shortcut icon">
+        <meta charset="UTF-8">
+        <link href="/MikaGithubtest/Vliegtickets/Styles.css" rel="stylesheet">
+    </head>
 
-<body>
-  Koop hier goedkope vliegtickets
-<form action="idk.php" method="post">
-  <input list="landen">
-<datalist id="landen">
-    <?php
-  $query = mysqli_query($conn,"SELECT Land FROM myTable");
-  $land = mysqli_fetch_all($query,MYSQLI_ASSOC);
-  foreach ($land as $key => $value) {
-    foreach ($value as $key => $v) {
-      echo "<option value='".$v."'>";
-    }
-  }
-  ?>
-</datalist>
-  <input type="submit">
-</form>
+    <body>
+        Koop hier goedkope vliegtickets
+        <form action="idk.php" method="post">
+            <input type="checkbox" checked name="Retour" id="Retour"><label for="Retour">Retour</label><br>
+            <input list="landen" name="Vertrek" id="Vertrek"><label for="Vertrek">Vertrek</label><br>
+            <input type="date" name="Terug" id="Terug" class="TerugCl"><label for="Terug" class="TerugCl">Terug<br></label>
+            <input list="landen" name="Aantkomst" id="Aankomst"><label for="Aankomst"> Aankomst</label><br>
+            <datalist id="landen">
+                <option value="1"></option>
+                <option value="2"></option>
+            </datalist>
+            <input type="date" name="DatumVlucht" id="DatumVLuchtheen"><label for="DatumVLuchtheen">Datum vlucht heen</label><br>
+
+            <input type="submit" name="Zoek een vlucht">
+        </form>
 
 
-</body>
+    </body>
+    <!-- http://stackoverflow.com/questions/25867236/change-div-color-with-css-checked-selector
+http://stackoverflow.com/questions/16989585/css-3-slide-in-from-left-transition
 
+-->
 </html>
