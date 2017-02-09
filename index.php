471ddf4555b669f4cc5dcb9b7bed4ed22ccc1809
MikaZeilstra <mika.zeilstra@gmail.com>
2017-02-09 14:02:04 +0100

    Terug

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ include 'config.php';
         <form action="idk.php" method="post">
             <input type="checkbox" checked name="Retour" id="Retour"><label for="Retour">Retour</label>
             <input list="landen" name="Vertrek" id="Vertrek"><label for="Vertrek">Vertrek</ label><br>
-            <input list="landen" name="Terug"
+            <input list="landen" name="Terug" id="Terug"><label for="Terug">Terug</label>
             <input list="landen" name="Aantkomst" id="Aankomst"><label for="Aankomst"> Aankomst</label><br>
             <datalist id="landen">
-                <option value="1">
+                <option value="1"></option>
                 <option value="2"></option>
             </datalist>
             <input type="date" name="DatumVlucht" id="DatumVLuchtheen"><label for="DatumVLuchtheen">Datum vlucht heen</label>
