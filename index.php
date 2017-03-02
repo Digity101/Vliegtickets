5c3e687c53b4ef5eda42603c88bc42b514098e70
Digity101 <danny.gia.an.thai@gmail.com>
2017-03-02 21:04:21 +0100

    suggesties

--- index.php
+++ index.php
@@ -5,13 +5,6 @@ include 'config.php';
 session_start();
 ?>
 
-<?php
-// Maakt verbinding
-include 'config.php';
-// Start the session
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -19,13 +12,6 @@ session_start();
         <title>Geen scam</title>
         <link href="icon.ico" rel="shortcut icon">
         <meta charset="UTF-8">
-        <link href="Styles.css" rel="stylesheet">
-        <script src="/jquery-3.1.1.min.js"></script>
-        <script src="jquery.cycle.lite.js"></script>
-        <script src="JSpag1.js"></script>
-        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
     </head>
 
 
@@ -93,8 +79,11 @@ for (var i = 0; i < inputs.length; i++) {
   });
 }
 });
-
 </script>
+        <link href="Styles.css" rel="stylesheet">
+        <script src="/jquery-3.1.1.min.js"></script>
+        <script src="jquery.cycle.lite.js"></script>
+        <script src="JSpag1.js"></script>
 
 <?php
 mysqli_close($conn);
